6af43cdd4bfc84d2d6dfcff954277ca65e039dce
Ficenes <pficenes@gmail.com>
2021-01-10 20:02:41 +0100

    Zmiany

--- Project/project/stores/forms.py
+++ Project/project/stores/forms.py
@@ -26,3 +26,14 @@ class StoresQuantityForm(forms.ModelForm):
             'quantity': '',
             'price': '',
         }
+        widgets = {
+            'area': forms.TextInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'country': forms.TextInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'city': forms.TextInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'store_name': forms.TextInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'sku': forms.TextInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'product_description': forms.TextInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'invoice_number': forms.TextInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'quantity': forms.NumberInput(attrs={'class' : 'Stores_Quantity_Form'}),
+            'price': forms.NumberInput(attrs={'class' : 'Stores_Quantity_Form'}),
+        }
