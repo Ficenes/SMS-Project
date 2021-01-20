0d318306d616783ff9d9b65481e5e8994817d62c
Ficenes <pficenes@gmail.com>
2021-01-20 23:37:56 +0100

    Invoices do zrobienia

--- Project/project/invoices/views.py
+++ Project/project/invoices/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from basket.models import Basket
 from .models import Invoices
+from django_pivot.pivot import pivot
 
 
 def show_invoices(request):
-    query_results = Invoices.objects.all()
+    inv_base = Invoices.objects.all()
+    pivot_table = pivot(Invoices, 'invoice_number', 'quantity', 'value')
     n_objects = Basket.objects.all().count()
     if request.method == "POST":
         instance = Invoices.objects.filter(invoice_number = request.POST['form_invoice_number'])
         instance.delete()
     context = {
-        'query_results' : query_results,
+        'inv_base' : inv_base,
         'n_objects' : n_objects,
+        'pivot_table' : pivot_table,
     }
     return render(request, "invoices.html", context)
--- Project/project/shop/views.py
+++ Project/project/shop/views.py
@@ -8,27 +8,35 @@ def show_shop(request):
     query_results = Shop.objects.all()
     n_objects = Basket.objects.all().count()
     form = ShopQuantityForm()
-    error = ''
     if request.method == 'POST':
         form = ShopQuantityForm(request.POST or None)
         if form.is_valid():
-            Basket.objects.create(
-                pic = request.POST['form_pic'],
-                sku = request.POST['form_sku'],
-                product_description = request.POST['form_product_description'],
-                quantity = request.POST['quantity'],
-                price = request.POST['form_price'],
-                value = float(request.POST['form_price']) * float(request.POST['quantity']),
-            )
+            in_basket = request.POST['form_sku']
+            req_quantity = float(request.POST['quantity'])
+            req_value = float(request.POST['form_price']) * float(request.POST['quantity'])
+            if Basket.objects.filter(sku=in_basket).count() != 0:
+                Basket.objects.filter(sku=in_basket).update(
+                    quantity = req_quantity + float(Basket.objects.filter(sku=in_basket)[0].quantity),
+                    value = req_value + float(Basket.objects.filter(sku=in_basket)[0].value),
+                    )
+            else:
+                Basket.objects.create(
+                    pic = request.POST['form_pic'],
+                    sku = request.POST['form_sku'],
+                    product_description = request.POST['form_product_description'],
+                    quantity = request.POST['quantity'],
+                    price = request.POST['form_price'],
+                    value = float(request.POST['form_price']) * float(request.POST['quantity']),
+                )
             context = {}
             return render(request, "addedToBasket.html", context)
         else:
-            error = 'Hello'
             form = ShopQuantityForm()
+            context = {}
+            return render(request, "error.html", context)
     context = {
         'form' : form,
         'query_results' : query_results,
-        'error' : error,
         'n_objects' : n_objects,
     }
     return render(request, "shop.html", context)
